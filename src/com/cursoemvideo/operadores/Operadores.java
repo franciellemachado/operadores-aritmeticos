4017c89e057f5ba0cf4300a56287a2be951be020
Francielle <franciellealmeidaa@hotmail.com>
2021-02-01 11:02:22 -0300

    Primeiro commit

---
+++ src/com/cursoemvideo/operadores/Operadores.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cursoemvideo.operadores;
+
+/**
+ *
+ * @author alexd
+ */
+public class Operadores {
+    
+    
+    
+    public static void main(String[]args) {
+        //TODO code application logic here
+        /*int n1 = 3;
+        int n2 = 5;
+        float n= (n1 + n2)/2;
+        System.out.println("A média é igual a" + m);*/
+        
+//        int numero = 10;
+//        int valor = 4 + numero --;
+//        System.out.println(valor); //         
+//        System.out.println(numero); // 
+
+//int x = 4;
+//x *= 2; //x = x * 2 
+//System.out.println(x);
+
+//int x = 4;
+//x +=2; //x = x + 2
+//System.out.println(x);
+
+//float v = 8.3f;
+//int ar =(int) Math.floor(v);//arredonda p baixo
+//System.out.println(ar); 
+
+//float v = 8.3f;
+//int ar = (int) Math.ceil(v); //arredonda p cima 
+//System.out.println(ar);
+
+//float v = 8.5f;
+//int ar = (int) Math.round(v);//arredonda aritmeticamente
+//System.out.println(ar);
+
+//double ale = Math.random();//número aleatorio de 0 e 1
+//System.out.println(ale);
+
+//double ale = Math.random();
+//int n =(int) (5 + ale *(10-5)); //número aleatorio de 5 ate 10
+//System.out.println(n);
+
+double ale = Math.random();
+int n =(int) (15 + ale *(50-15)); //
+System.out.println(n);
+
+
+        
+        
+    }
+}
+
